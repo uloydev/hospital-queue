e9b6df07487e91df0e4cd1855ad4d3c041c9765e
Dicky Nugeraha <dicky.dian1@gmail.com>
2022-07-12 00:19:51 +0700

    [FINAL] - UI

--- frontend/src/components/Poli/RealtimeAntrean.js
+++ frontend/src/components/Poli/RealtimeAntrean.js
@@ -1,7 +1,51 @@
 import React from "react";
+import "./Button.css";
 
-const RealtimeAntrean = () => {
-  return <div>RealtimeAntrean</div>;
+const RealtimeAntrean = (props) => {
+  const doneQueueHandler = () => {
+    props.onDoneQueue(props.polyId);
+  };
+  const notifyQueueHandler = () => {
+    props.onNotifyQueue(props.polyId);
+  };
+  return (
+    <div>
+      <h4
+        style={{
+          marginTop: "20px",
+          marginBottom: "30px",
+        }}
+      >
+        Nomor Antrean yang sedang berlangsung
+      </h4>
+      <div>
+        <span className="square">{props.currNumber}</span>
+        <span className="desc">
+          {props}
+          {props.minutes}:{props.seconds}
+        </span>
+      </div>
+      <div>
+        <button
+          style={{
+            background: "#D900FD",
+            color: "white",
+            padding: "0.5rem 1.2rem",
+            marginBottom: "15px",
+            marginTop: "40px",
+          }}
+          onClick={notifyQueueHandler}
+        >
+          Notifikasi Pemeriksaan
+        </button>
+      </div>
+      <div>
+        <button className="success white" onClick={doneQueueHandler}>
+          Selesai
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RealtimeAntrean;
